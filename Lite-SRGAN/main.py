cb829850fe4a39ddccad31f3c886b183e5ad6f41
vk8540002229-dev <vk8540002229@gmail.com>
2025-10-23 01:15:28 +0530

    Update main.py

--- Lite-SRGAN/main.py
+++ Lite-SRGAN/main.py
@@ -6,25 +6,21 @@ import os
 from tqdm import tqdm
 
 # -----------------------------
-# Argument Parser (unchanged)
+# Argument Parser (same)
 # -----------------------------
 parser = argparse.ArgumentParser(description='light-SRGAN training script.')
 
-# Dataloader args
 parser.add_argument('--images_dir', default=r"/content/drive/MyDrive/images_001/original_images",
                     type=str, help='provide a path containing all the data')
 parser.add_argument('--img_width', default=128, type=int)
 parser.add_argument('--img_height', default=128, type=int)
-
-# Model args
-parser.add_argument('--upsampling_blocks', default=1, type=int,
-                    help='Number of upsampling blocks for upscaling.')
+parser.add_argument('--upsampling_blocks', default=1, type=int)
 parser.add_argument('--batch_size', default=8, type=int)
 parser.add_argument('--lr', default=1e-4, type=float)
 parser.add_argument('--decay_steps', default=50000, type=int)
 parser.add_argument('--decay_rate', default=0.1, type=float)
 parser.add_argument('--epochs', default=100, type=int)
-parser.add_argument('--pretraining_epochs', default=100, type=int)
+parser.add_argument('--pretraining_epochs', default=9, type=int)
 parser.add_argument('--generator_weights', default=None, type=str)
 parser.add_argument('--discriminator_weights', default=None, type=str)
 
@@ -37,9 +33,12 @@ BASE_DIR = '/content/drive/MyDrive/LiteSRGAN_data'
 MODELS_DIR = os.path.join(BASE_DIR, 'models')
 GEN_DIR = os.path.join(BASE_DIR, 'generatedTrails')
 CKPT_DIR = os.path.join(BASE_DIR, 'checkpoints')
+PRETRAIN_DIR = os.path.join(CKPT_DIR, 'pretrain')
 
-for d in [BASE_DIR, MODELS_DIR, GEN_DIR, CKPT_DIR]:
+for d in [BASE_DIR, MODELS_DIR, GEN_DIR, CKPT_DIR, PRETRAIN_DIR]:
     os.makedirs(d, exist_ok=True)
+
+EPOCH_TRACKER = os.path.join(PRETRAIN_DIR, 'pretrain_epoch.txt')
 
 print(f"✅ All training data will be stored under: {BASE_DIR}")
 
@@ -57,23 +56,42 @@ lite_SRGAN_engine = LiteSRGAN_engine(args, lite_SRGAN)
 print("last layer shape of discriminator: ", lite_SRGAN.discriminator.output_shape)
 
 # -----------------------------
-# Pretraining Checkpoint
+# Pretraining Checkpoint + Manual Epoch Tracker
 # -----------------------------
-pre_ckpt = tf.train.Checkpoint(generator=lite_SRGAN.generator)
-pre_ckpt_manager = tf.train.CheckpointManager(pre_ckpt, os.path.join(CKPT_DIR, 'pretrain'), max_to_keep=3)
+pretrain_epoch = tf.Variable(0, dtype=tf.int64, trainable=False)
+pre_ckpt = tf.train.Checkpoint(generator=lite_SRGAN.generator, epoch=pretrain_epoch)
+pre_ckpt_manager = tf.train.CheckpointManager(pre_ckpt, PRETRAIN_DIR, max_to_keep=3)
+
+# Read epoch from file if available
+if os.path.exists(EPOCH_TRACKER):
+    with open(EPOCH_TRACKER, 'r') as f:
+        saved_epoch = int(f.read().strip())
+    pretrain_epoch.assign(saved_epoch)
+else:
+    saved_epoch = 0
 
 if pre_ckpt_manager.latest_checkpoint:
-    pre_ckpt.restore(pre_ckpt_manager.latest_checkpoint)
-    print(f"🔁 Resumed generator pretraining from {pre_ckpt_manager.latest_checkpoint}")
+    pre_ckpt.restore(pre_ckpt_manager.latest_checkpoint).expect_partial()
+    print(f"🔁 Resumed generator pretraining from {pre_ckpt_manager.latest_checkpoint} (epoch {saved_epoch})")
 else:
     print("🚀 Starting generator pretraining from scratch...")
 
 # -----------------------------
-# Pretraining Loop
+# Pretraining Loop (resumable)
 # -----------------------------
-for i in range(args.pretraining_epochs):
+start_epoch = int(pretrain_epoch.numpy())
+for i in range(start_epoch, args.pretraining_epochs):
+    print(f"------------- Pre-training epoch {i} -------------")
     lite_SRGAN_engine.generator_pretraining(datagen, i)
-    pre_ckpt_manager.save()
+    pretrain_epoch.assign_add(1)
+
+    # Save checkpoint
+    pre_ckpt_manager.save(checkpoint_number=int(pretrain_epoch.numpy()))
+
+    # Save epoch to file
+    with open(EPOCH_TRACKER, 'w') as f:
+        f.write(str(int(pretrain_epoch.numpy())))
+
     print(f"💾 Saved pretraining checkpoint after epoch {i+1}")
 
 print("------------- End of generator pre-training -------------")
